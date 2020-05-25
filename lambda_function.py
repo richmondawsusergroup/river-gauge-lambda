69ff37110638ebf77cea66b7e254dbc4fa0bbd83
richmondawsusergroup <65854519+richmondawsusergroup@users.noreply.github.com>
2020-05-24 20:37:02 -0400

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -2,11 +2,16 @@ import requests
 import xmltodict
 import json
 
-def lambda_handler(event, context):
+def current_stage():
     r = requests.get('https://water.weather.gov/ahps2/hydrograph_to_xml.php?gage=rmdv2&output=xml')
-    dict = xmltodict.parse(r.content)
-    data = dict['site']['observed']['datum'][000]['primary']['#text']
-    return {
-        'statusCode': 200,
-        'body': json.dumps(float(data))
-    }
+    dict = xmltodict.parse(r.content)   
+    current_height = dict['site']['observed']['datum'][000]['primary']['#text']
+    future_height = dict['site']['forecast']['datum'][000]['primary']['#text']
+    is_rising = (float(current_height) <= float(future_height))
+    if is_rising:
+        trend = "and rising"
+    else:
+        trend = "and falling"
+        
+    speak_output = "The James River is currently " + current_height + " feet " + trend +  " at the Westham gauge."    
+    return speak_output
